5d94bb6178342b71fe8688e9063dc2bf8bc72302
unknown <vaibhavraut1331@gmail.com>
2024-06-03 17:12:05 +0530

    updated eda2 is added

--- eda_2.py
+++ eda_2.py
@@ -1,8 +1,9 @@
 print('Exploratory Data Analysis')
 
-print('Hello World!!!')
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+df=pd.read_csv('Data.csv')
+df=df.astype(int)
+new_val=df.groupby('col1')['col2'].sum(axis=1)
