366d51345ff431c95d6383e2f388c41152f56935
unknown <vaibhavraut1331@gmail.com>
2024-06-03 22:46:48 +0530

    deleting file

--- training.py
+++
@@ -1,9 +0,0 @@
-print('Model Training Process')
-
-from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import AdaBoostClassifier
-
-log_clf = LogisticRegression()
-
-adb_clf = AdaBoostClassifier()
